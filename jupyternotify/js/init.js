3507b31d53f6f6bf6cd8e343fa8e557830a35f75
Corentin Cadiou <contact@cphyc.me>
2019-05-01 11:38:02 +0200

    Inject jquery if missing

--- jupyternotify/js/init.js
+++ jupyternotify/js/init.js
@@ -7,3 +7,9 @@ if (!("Notification" in window)) {
         }
     })
 }
+
+if(!window.jQuery) {
+    var jq = document.createElement('script');
+    jq.src = "//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+    document.getElementsByTagName('head')[0].appendChild(jq);
+}
